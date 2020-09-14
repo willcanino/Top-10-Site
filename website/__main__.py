fd165d0a67cf7003e085843c3ccf9f9baae80d89
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-09-14 14:20:39 -0700

    Added debugging in __main__.py when users run "python website"

--- website/__main__.py
+++ website/__main__.py
@@ -1,2 +1,8 @@
-from website import app
-app.run(debug=True)
+try:
+    from website import app
+    app.run(debug=True)
+except ModuleNotFoundError:
+    import click
+    click.echo(click.style('To avoid this error use ', fg='yellow')
+                + click.style('python -m website', fg='yellow', bold=True))
+    raise
