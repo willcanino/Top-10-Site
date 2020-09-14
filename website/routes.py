395dc12d1579999b6f4e9818a6d4a2b138299d79
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-09-14 12:39:34 -0700

    Created a basic routes.py file

---
+++ website/routes.py
@@ -0,0 +1,38 @@
+import flask
+from website import app
+
+@app.route('/')
+def home():
+    return flask.render_template('index.html')
+
+@app.route('/2020-hp')
+def _2020_hp():
+    return flask.render_template('2020-hp.html')
+
+@app.route('/about')
+def about():
+    return flask.render_template('about-us.html')
+
+@app.route('/artist-home')
+def artist_home():
+    return flask.render_template('artist-homepage.html')
+
+@app.route('/categories')
+def categories():
+    return flask.render_template('categories.html')
+
+@app.route('/contact')
+def contact():
+    return flask.render_template('contact-us.html')
+
+@app.route('/login')
+def login():
+    return flask.render_template('login-page.html')
+
+@app.route('/sign-up')
+def signup():
+    return flask.render_template('sign-up-page.html')
+
+@app.route('/thank-you-for-contacting')
+def thanks_contact():
+    return flask.render_template('thanks-for-contacting.html')
