be6e2ec95e7328a04b9eaac59fc4b66ff53955d2
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-09-14 15:16:54 -0700

    Fixed typo in comment in __main__.py

--- website/__main__.py
+++ website/__main__.py
@@ -5,5 +5,5 @@ except ModuleNotFoundError:
     import click
     click.echo(click.style('To avoid this error use ', fg='yellow')
                 + click.style('python -m website', fg='yellow', bold=True))
-    # raise keyword will raise whatever error is except above
+    # raise keyword will raise whatever error is excepted above
     raise
