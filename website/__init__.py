450c781c418113c6c2a39740f6b2ed8a165ae1d6
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-09-14 17:35:08 -0700

    An email is now sent when someone fills out the Contact Us form

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_mail import Mail
 from dotenv import dotenv_values
 
 # Load environment variables
 # Flask will autoload variables from .env file when using
 # `flask run` command or running the `app.py` file
 app = Flask(__name__)
-app.config.from_mapping(**dotenv_values())
+config_dict = dotenv_values()
+del config_dict['contact_page_email']
+app.config.from_mapping(**config_dict)
+
+mail = Mail(app)
 
 # Weird circular imports going on here
 # but we must import routes **AFTER** we create
--- website/routes/post_routes.py
+++ website/routes/post_routes.py
@@ -3,17 +3,18 @@ import pathlib
 import datetime
 import json
 from flask import url_for, redirect
-from website import app
+from flask_mail import Message
+from dotenv import dotenv_values
+from website import app, mail
+
 
 @app.route('/handle-contact-us-data', methods=['POST'])
 def handle_contact_us_data():
-    post_request = flask.request.form
+    post_request = flask.request.form.to_dict()
+    post_request.pop('button')
     contact_us_dir = pathlib.Path('./contact-us')
     contact_us_dir.mkdir(exist_ok=True)
-    unparsed = post_request.to_dict(flat=False)
-    unparsed.pop('button')
-    parsed = {key: val[0] if len(val) == 1 else val for key, val in unparsed.items()}
-    if any(parsed.values()):
+    if any(post_request.values()):
         # This is `slightly` risky file naming because if the user makes another POST request
         # within one second of sending there previous one, there will be filenaming conflicts.
         # With the current implementation, the old file will just be overwritten,
@@ -21,6 +22,15 @@ def handle_contact_us_data():
         # to the existing file.
         post_data_loc = contact_us_dir / f"{format(datetime.datetime.today(), '%Y-%m-%d_%I.%M.%S.%p')}.json"
         with post_data_loc.open(mode='w') as file:
-            json.dump(parsed, file, indent=4)
+            json.dump(post_request, file, indent=4)
+
+        # Send an email when someone fill outs the `Contact Us` form
+        msg = Message(subject=f"{post_request['fname']} {post_request['lname']} has fill out the Contact Us form!",
+                      body=(f"First Name: {post_request['fname']}\n"
+                            f"Last Name: {post_request['lname']}\n"
+                            f"Reason for Contacting: {post_request['reason']}\n"
+                            f"Email address: {post_request['eaddress']}"),
+                      recipients=[dotenv_values()['contact_page_email']])
+        mail.send(msg)
 
     return redirect(url_for('thanks_contact'))
