22eb1955194ccc93291112b7ca7b020de2d05d62
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-09-16 19:17:22 -0700

    Fixed broken threading in the Contact Us page

--- website/routes/post_routes.py
+++ website/routes/post_routes.py
@@ -2,39 +2,39 @@ import flask
 import pathlib
 import datetime
 import json
+import threading
 from flask import url_for, redirect
 from flask_mail import Message
 from dotenv import dotenv_values
 from website import app, mail
-from concurrent.futures import ThreadPoolExecutor
 
 
 @app.route('/handle-contact-us-data', methods=['POST'])
 def handle_contact_us_data():
-    with ThreadPoolExecutor() as executor:
-        executor.submit(store_and_send_contact_data)
+    contact_data_thread = threading.Thread(target=store_and_send_contact_data, args=(flask.request.form.to_dict(),))
+    contact_data_thread.start()
     return redirect(url_for('thanks_contact'))
 
-def store_and_send_contact_data():
-    post_request = flask.request.form.to_dict()
-    post_request.pop('button')
-    contact_us_dir = pathlib.Path('./contact-us')
-    contact_us_dir.mkdir(exist_ok=True)
-    if any(post_request.values()):
-        # This is `slightly` risky file naming because if the user makes another POST request
-        # within one second of sending there previous one, there will be filenaming conflicts.
-        # With the current implementation, the old file will just be overwritten,
-        # mainly so that the data is consistent, as opposed to appending the data
-        # to the existing file.
-        post_data_loc = contact_us_dir / f"{format(datetime.datetime.today(), '%Y-%m-%d_%I.%M.%S.%p')}.json"
-        with post_data_loc.open(mode='w') as file:
-            json.dump(post_request, file, indent=4)
+def store_and_send_contact_data(post_request):
+    with app.app_context():
+        post_request.pop('button')
+        contact_us_dir = pathlib.Path('./contact-us')
+        contact_us_dir.mkdir(exist_ok=True)
+        if any(post_request.values()):
+            # This is `slightly` risky file naming because if the user makes another POST request
+            # within one second of sending there previous one, there will be filenaming conflicts.
+            # With the current implementation, the old file will just be overwritten,
+            # mainly so that the data is consistent, as opposed to appending the data
+            # to the existing file.
+            post_data_loc = contact_us_dir / f"{format(datetime.datetime.today(), '%Y-%m-%d_%I.%M.%S.%p')}.json"
+            with post_data_loc.open(mode='w') as file:
+                json.dump(post_request, file, indent=4)
 
-        # Send an email when someone fill outs the `Contact Us` form
-        msg = Message(subject=f"{post_request['fname']} {post_request['lname']} filled out the Contact Us form!",
-                      body=(f"First Name: {post_request['fname']}\n"
-                            f"Last Name: {post_request['lname']}\n"
-                            f"Reason for Contacting: {post_request['reason']}\n"
-                            f"Email address: {post_request['eaddress']}"),
-                      recipients=[dotenv_values()['contact_page_email']])
-        mail.send(msg)
+            # Send an email when someone fill outs the `Contact Us` form
+            msg = Message(subject=f"{post_request['fname']} {post_request['lname']} filled out the Contact Us form!",
+                          body=(f"First Name: {post_request['fname']}\n"
+                                f"Last Name: {post_request['lname']}\n"
+                                f"Reason for Contacting: {post_request['reason']}\n"
+                                f"Email address: {post_request['eaddress']}"),
+                          recipients=[dotenv_values()['contact_page_email']])
+            mail.send(msg)
